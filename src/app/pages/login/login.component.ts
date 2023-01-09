7e18fa34625fe58ef0c47fea6a4cc02db0b735fe
Nitha <nitha.spst@gmail.com>
2023-01-09 18:46:59 +0700

    initial commit

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  info: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {}
+
+  handleSubmit(value: any) {
+    const body = {
+      username: value.username,
+      password: value.password,
+    };
+    this.http
+      .post(`http://212.80.212.177:9001/api/v1/auth/login`, body)
+      .subscribe(
+        (res: any) => {
+          this.info = JSON.stringify(res.data, undefined, 2);
+          console.log('success', res);
+        },
+        (err) => {
+          this.info = JSON.stringify(err.error, undefined, 2);
+          console.log('error', err);
+        }
+      );
+  }
+}
